582e4b99000c5a8a49de291f3fb09e7d89eb20db
gabrielsuvia <ggabrielomar123@hotmail.com>
2024-10-22 19:20:02 -0400

    corregidoPoo

--- punto3.js
+++ punto3.js
@@ -1,23 +1,28 @@
 //lista de tareas
-const tareas = []
 
+class AppTareas{
 
-function mostrarOpciones(){
-console.log("\n--- Opciones ----");
-console.log("1. Añadir tarea");
-console.log("2. Eliminar tarea");
-console.log("3. tarea completada");
-console.log("4. tarea pendiente");
-}
+    constructor(){
+      this.tareas = [];
+    }
 
-//añadir tarea
-function añadirTarea(){
+    mostrarOpciones(){
+    console.log("\n--- Opciones ----");
+    console.log("1. Añadir tarea");
+    console.log("2. Eliminar tarea");
+    console.log("3. tarea completada");
+    console.log("4. tarea pendiente");
+    }
+        
+   //añadir tarea
+    añadirTarea(){
     const tarea = prompt("escribe una tarea: ");
     tareas.push(tarea);
     console.log(`tarea agregada: ${tarea}`);
-}
-//eliminar tarea
-function eliminarTarea(){
+    }
+
+   //eliminar tarea
+    eliminarTarea(){
     tareas.forEach((tarea,indice)=>{
       console.log(`${indice+1}:${tarea}`)
     });
@@ -27,6 +32,38 @@ function eliminarTarea(){
     }else{
         console.log("Número de tarea inválido");
     }
+    }
+
+    //marcar tarea como completada
+//function tareaCompletada(){
+//}
+
+// Función principal
+ 
+
+
+ejecutar() {
+    while (true) {
+      mostrarMenu();
+      const opcion = parseInt(prompt("Ingrese una opción: "));
+      switch (opcion) {
+        case 1:
+          agregarTarea();
+          break;
+        case 2:
+          eliminarTarea();
+          break;
+        default:
+            console.log("opcion invalida");
+        }
+    }
 }
 
-//
+}
+
+
+
+
+
+
+
