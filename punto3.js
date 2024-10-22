cd5ba2eb0176a976d05dbd1595c5d8124dc812bc
gabrielsuvia <ggabrielomar123@hotmail.com>
2024-10-22 18:47:40 -0400

    añadirTarea

--- punto3.js
+++ punto3.js
@@ -1,1 +1,19 @@
 //lista de tareas
+const tareas = []
+
+
+function mostrarOpciones(){
+console.log("\n--- Opciones ----");
+console.log("1. Añadir tarea");
+console.log("2. Eliminar tarea");
+console.log("3. tarea completada");
+console.log("4. tarea pendiente");
+}
+
+//añadir tarea
+function añadirTarea(){
+    const tarea = prompt("escribe una tarea: ");
+    tareas.push(tarea);
+    console.log(`tarea agregada: ${tarea}`);
+}
+
